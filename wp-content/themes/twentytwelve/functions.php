d17274a47a3e4fb8e6c07dee267efd53c0caabdf
asael2 <asael2@gmail.com>
2013-05-23 01:16:55 -0300

    en integracion en web

--- wp-content/themes/twentytwelve/functions.php
+++ wp-content/themes/twentytwelve/functions.php
@@ -465,4 +465,24 @@ function customer_reference(){
 };
 add_filter('gform_field_value_uuid', 'customer_reference');
 
+/* Disable the Admin Bar. */
+add_filter( 'show_admin_bar', '__return_false' );
 
+ function yoast_hide_admin_bar_settings() {
+?>
+	<style type="text/css">
+		.show-admin-bar {
+			display: none;
+		}
+	</style>
+<?php
+}
+
+function yoast_disable_admin_bar() {
+    add_filter( 'show_admin_bar', '__return_false' );
+    add_action( 'admin_print_scripts-profile.php', 
+         'yoast_hide_admin_bar_settings' );
+}
+add_action( 'init', 'yoast_disable_admin_bar' , 9 );
+
+?>
