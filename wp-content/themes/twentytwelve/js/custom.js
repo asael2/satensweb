23dc8179f491effaf50f9dd92d3438c884660fab
asael2 <asael2@gmail.com>
2013-07-06 18:48:19 -0300

    LARES
    comienzo dessarrollo nuevo

--- wp-content/themes/twentytwelve/js/custom.js
+++ wp-content/themes/twentytwelve/js/custom.js
@@ -1,37 +1,42 @@
-var reporte = {
+var respuestas, leadId;
+
+
+var reporte={};
+
+
+reporte : {
 	
-	var respuestas, 
-	var fn = null;
-	var leadId = $.urlParam('leadid');
-	
-	//Fetch Parameter from URL
-	$.urlParam = function(name){
-		var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(window.location.href);
-		if (results==null){
-			return null;
-		}else{
-			return results[1] || 0;
-		}
-	}
+	init : function(){
+		//Fetch Parameter from URL
+		$.urlParam = function(name){
+			var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(window.location.href);
+			if (results==null){
+				return null;
+			}else{
+				return results[1] || 0;
+			}
+		};
+		
+		leadId = $.urlParam('leadid');
 
-
-
-	$.get('/servicio.php?leadid='+leadId).done(function(data) {
-		respuestas = data.r;
-		console.log(respuestas);
-		console.log("Total registros :: "+respuestas.length);
-		startDraw();
-	});
+		$.get('/servicio.php?leadid='+leadId).done(function(data) {
+			respuestas = data.r;
+			console.log(respuestas);
+			console.log("Total registros :: "+respuestas.length);
+			startDraw();
+		});
+	}	
 
 }
 
 
 
-var pregunta = function(fnumber){
-	var label = $(".entry-view-field-name").eq(fnumber).text();
+var pregunta = function(DOMnumber){
+	var label = $(".entry-view-field-name").eq(DOMnumber).text();
 	//console.log(label);
 	return label;
 }
+
 var respondido = function(fieldN){
 	var fn, fvalue, fieldNumber, lares;
 	fieldNumber = fieldN;
@@ -47,7 +52,7 @@ var respondido = function(fieldN){
 }
 
 
-function eLead(label, value){
+function studentBasicData(label, value){
 
 	$("#leadData").append("<li>"+label+" : "+value+"</li>")
 }
@@ -94,28 +99,29 @@ function laDataTable() {
 	var table = new google.visualization.Table(document.getElementById('visualizationb'));
 	table.draw(data, {showRowNumber: false});
 }
+
 //Global Scope eof///////////////////////////////////
 
 
 function startDraw(){
 
 	//SERAID, Field numbers 1
-	eLead(pregunta(0) , respondido(1));
+	studentBasicData(pregunta(0) , respondido(1));
 	
 	//Info del estudiante,Field numbers 3-6
-    eLead(pregunta(1) , respondido(3));
-    eLead(pregunta(2) , respondido(4));
-    eLead(pregunta(3) , respondido(5));
-    eLead(pregunta(4) , respondido(6));
-    eLead(pregunta(5) , respondido(7));
-    eLead(pregunta(6) , respondido(8));
-    eLead(pregunta(7) , respondido(9));
+    studentBasicData(pregunta(1) , respondido(3));
+    studentBasicData(pregunta(2) , respondido(4));
+    studentBasicData(pregunta(3) , respondido(5));
+    studentBasicData(pregunta(4) , respondido(6));
+    studentBasicData(pregunta(5) , respondido(7));
+    studentBasicData(pregunta(6) , respondido(8));
+    studentBasicData(pregunta(7) , respondido(9));
    
  	//CHART 1 
 	siNo(pregunta(17), respondido(22) )
 
 	//DATATABLE 1      
-	laDataTable();
+	//laDataTable();
 
 		
 
@@ -123,7 +129,7 @@ function startDraw(){
 
 
 
-
+/*
 function getCBValues(eNum){
 	var b1 = eNum;
 	var b2 = b1+1;
@@ -146,6 +152,7 @@ function getCBValues(eNum){
 $(function(){
 	//$(".entry-detail-view").hide(); 
 	
+/*
 	$.each(".entry-view-field-name", "body", function(){
 		console.log(this.text())
 	})
@@ -153,6 +160,9 @@ $(function(){
 	$(".entry-view-field-name").each(function(indice, elemento) {
   		console.log($(elemento).text());
 	});
+*/
+
+	reporte.init()
 
 	//SERAiD
 	$(".gform_wrapper .readonly input").attr('readonly', 'readonly').css("background","#CCC");
