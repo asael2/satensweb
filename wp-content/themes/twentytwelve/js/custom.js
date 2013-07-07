05e64946c4478aad2204676e03024bac2c794bd3
asael2 <asael2@gmail.com>
2013-07-07 15:27:12 -0300

    de 10 falta definir elSelect

--- wp-content/themes/twentytwelve/js/custom.js
+++ wp-content/themes/twentytwelve/js/custom.js
@@ -16,7 +16,7 @@ var reporte = {
 
 		$.get('/servicio.php?leadid='+leadId).done(function(data) {
 			respuestas = data.r;
-			//console.log(respuestas);
+			console.log(respuestas);
 			console.log("Total registros :: "+respuestas.length);
 			startDraw();
 		});
@@ -43,12 +43,13 @@ var respondido = function(fieldN){
     return lares;
 }
 
+//Classes///////////////////////////////////
 function studentBasicData(label, value){
 
 	$("#leadData").append("<li>"+label+" : "+value+"</li>")
 }
 
-function siNo(pregunta, respuesta) {
+function siNoColumnChart(pregunta, respuesta, targetDom) {
 	var data = google.visualization.arrayToDataTable([
 		['Sí | No = 0', 'Valor'],
 		['Respuesta',  parseInt(respuesta)],
@@ -63,21 +64,17 @@ function siNo(pregunta, respuesta) {
 		bar: {groupWidth: 100}
 	};
 
-	var chart = new google.visualization.ColumnChart(document.getElementById('visualization'));
+	var chart = new google.visualization.ColumnChart(document.getElementById(targetDom));
 	chart.draw(data, options);
 };
 
-function laDataTable(nRespuestas) {
-
-var data = google.visualization.arrayToDataTable(nRespuestas);
-
-
-
-	var table = new google.visualization.Table(document.getElementById('visualizationb'));
+function laDataTable(nRespuestas, targetDom) {
+	var data = google.visualization.arrayToDataTable(nRespuestas);
+	var table = new google.visualization.Table(document.getElementById(targetDom));
 	table.draw(data, {showRowNumber: false}); 
 }
 
-function velocimetros(nRespuestas) {
+function velocimetros(nRespuestas, targetDom) {
 	var data = google.visualization.arrayToDataTable(nRespuestas);
 	var options = {
 		width: 600, 
@@ -89,17 +86,31 @@ function velocimetros(nRespuestas) {
 		greenFrom: 2.51, greenTo: 3,
 		backgroundColor: {strokeWidth: 1, fill: 'white',},
 	};
-	var chart = new google.visualization.Gauge(document.getElementById('visualizationc'));
+	var chart = new google.visualization.Gauge(document.getElementById(targetDom));
 	chart.draw(data, options);
 }
+
+function pieChart(nRespuestas, pieTittle, targetDom) {
+	var data = google.visualization.arrayToDataTable(nRespuestas);
+
+	var options = {
+		title: pieTittle,
+		backgroundColor: {strokeWidth: 1, fill: 'white',},
+		legend: {position: 'bottom', alignment: 'start'}
+		};
+	var charte = new google.visualization.PieChart(document.getElementById(targetDom));
+	charte.draw(data, options);
+
+}
+
+
 //Global Scope eof///////////////////////////////////
 
 
 function startDraw(){
 
 	//SERAID, Field numbers 1
-	studentBasicData(pregunta(0) , respondido(1));
-	
+	studentBasicData(pregunta(0) , respondido(1));	
 	//Info del estudiante,Field numbers 3-6
     studentBasicData(pregunta(1) , respondido(3));
     studentBasicData(pregunta(2) , respondido(4));
@@ -108,50 +119,127 @@ function startDraw(){
     studentBasicData(pregunta(5) , respondido(7));
     studentBasicData(pregunta(6) , respondido(8));
     studentBasicData(pregunta(7) , respondido(9));
+	studentBasicData("Editar", 	(function(){
+			var editLink = $(".useredit").html()
+			console.log(editLink);
+			return editLink;
+		})());
    
- 	//CHART 1 
-	siNo(pregunta(17), respondido(22) )
+ //Form Auto Perfil Educativo del Estudiante
 
-	//DATATABLE 1      
+ 	//CHART
+	siNoColumnChart(pregunta(17), respondido(22), 'vSiNo-1')
+
+	//DATATABLE      
 	var dataSet1 = [
-
-		['Country', 'Area'],
-
-		['Consejero Escolar', 		(respondido(23.1) == "true") ], 
-		['Sicólogo', 				(respondido(23.2) == "true") ], 
-		['Tutoría', 				(respondido(23.3) == "true") ],
-		['Terapia de Habla',  		(respondido(23.4) == "true") ],
-		['Terapia Ocupacional',  	(respondido(23.5) == "true") ],
-		['Asistencia Tecnológica',	(respondido(23.6) == "true") ],
-		['Acomodos', 				(respondido(23.7) == "true") ],
-		['Otros',  					(respondido(23.8) == "true") ]
+			//Columnas
+			['Servicios que recibe en la escuela', 'Recibe'],
+			//Filas
+			['Consejero Escolar', 		(respondido(23.1) == "true") ], 
+			['Sicólogo', 				(respondido(23.2) == "true") ], 
+			['Tutoría', 				(respondido(23.3) == "true") ],
+			['Terapia de Habla',  		(respondido(23.4) == "true") ],
+			['Terapia Ocupacional',  	(respondido(23.5) == "true") ],
+			['Asistencia Tecnológica',	(respondido(23.6) == "true") ],
+			['Acomodos', 				(respondido(23.7) == "true") ],
+			['Otros',  					(respondido(23.8) == "true") ]
 	];
+	laDataTable(dataSet1, 'vDT-DS1');
 	
-
-
-laDataTable(dataSet1);	
-
-
-	// VELOCIMETROS 1
+	//VELOCIMETROS
 	var dataSet2 = [
 		['Destreza',	'Valor'],
 		['Lectura', 	parseInt(respondido(24))],
 		['Escritura',	parseInt(respondido(25))],
 		['Matemáticas',	parseInt(respondido(26))]
 	];
-	velocimetros(dataSet2);
+	velocimetros(dataSet2, 'vGVel-DS2');
 
-	//DATATABLE 2
-/*	var dataSet3 = [
-      ['Desconozco',			 	null, null, null],
-      ['Por debajo de los demás',	null, true, null],
-      ['Igual que los demás',  		true, null, null],
-	  ['Por encima de los demás',  	null, null, true]
+	//DATATABLE
+	
+	var dataSet3 = [
+		//Columnas
+		['Autocalificación ', 'Lectura', 'Escritura', 'Matematicas'],
+		
+		//Filas
+		['Desconozco',				(respondido(24) == 0), (respondido(25) == 0), (respondido(26) == 0)],
+      	['Por debajo de los demás', (respondido(24) == 1), (respondido(25) == 1), (respondido(26) == 1)],
+      	['Igual que los demás',  	(respondido(24) == 2), (respondido(25) == 2), (respondido(26) == 2)],
+	  	['Por encima de los demás', (respondido(24) == 3), (respondido(25) == 3), (respondido(26) == 3)]
 	];
-	laDataTable(dataSet3, 'Autocalificación');
-*/
+	laDataTable(dataSet3, 'vDT-DS3');
+	
+	//DATATABLE
+	
+	var dataSet4 = [
+		//Columnas
+		['Manera de confrontar necesidades académicas', 'Seleccion'],
+		//Filas
+		['Pienso en varias soluciones', 										(respondido(27.1) == "true") ],
+		['Busco consejo de otros', 												(respondido(27.2) == "true") ],
+		['Comienzo a resolver el probelma sin tener un plan', 					(respondido(27.3) == "true") ],
+		['Considero las cosas positivas y negativas del problema', 				(respondido(27.4) == "true") ],
+		['Considero otra solución cuando la primera idea no parece funcionar',	(respondido(27.5) == "true") ]
+	];
+	laDataTable(dataSet4, 'vDT-DS4');
 
+//Form Break<<<Intereses y Actividades Generales
+
+	//CHART 
+	siNoColumnChart(pregunta(23), respondido(29), 'vSiNo-2')
+	//CHART 
+	siNoColumnChart(pregunta(24), respondido(30), 'vSiNo-3')
+	//CHART
+	siNoColumnChart(pregunta(25), respondido(31), 'vSiNo-4')
+
+//Form Break<<<Intereses y Metas de Estudios Generales
+
+	//DATATABLE  
+
+	var dataSet5 = [
+		//Columnas
+		['Estudios que toma al presente o interesa tomar en la escuela superior', 'Seleccion'],
+		//Filas
+			['No Aplica', null],
+			['Programa de Preparación Universitaria', null],
+			['Programa General', true],
+			['Certificado Técnico / Vocaional',  null],
+			['Programa de Estudio y Trabajo',  null],
+			['Otro',  null],
+
+		['Pienso en varias soluciones', 										(respondido(27.1) == "true") ],
+		['Busco consejo de otros', 												(respondido(27.2) == "true") ],
+		['Comienzo a resolver el probelma sin tener un plan', 					(respondido(27.3) == "true") ],
+		['Considero las cosas positivas y negativas del problema', 				(respondido(27.4) == "true") ],
+		['Considero otra solución cuando la primera idea no parece funcionar',	(respondido(27.5) == "true") ]
+
+	];
+	laDataTable(dataSet5, 'vDT-DS5');
+
+	/*  
+	var dataSet5 = [
+			//Columnas
+			['Estudios que toma al presente o interesa tomar en la escuela superior', 'Seleccion'],
+			//Filas
+			['No Aplica', 								(respondido(33) == "true")],
+			['Programa de Preparación Universitaria', 	(respondido(33) == "true")],
+	        ['Programa General', 						(respondido(33) == "true")],
+	        ['Certificado Técnico / Vocaional',  		(respondido(33) == "true")],
+			['Programa de Estudio y Trabajo',  			(respondido(33) == "true")],
+			['Otro', 									(respondido(33) == "true")],
+	];
+	laDataTable(dataSet5, 'vDT-DS5');*/
+
+
+
+//vPC-DS7
+
+
+
+//end Draw 
 }
+
+
 
 		
 //ready
--- wp-content/themes/twentytwelve/leadReport.php
+++ wp-content/themes/twentytwelve/leadReport.php
@@ -16,21 +16,37 @@ LeadReport Template :)
 <!--Load the AJAX API--> 
 <script type="text/javascript" src="https://www.google.com/jsapi"></script> 
 <script type="text/javascript">
-	google.load('visualization', '1', {'packages':['corechart','geochart','table', 'gauge']});
+	google.load('visualization', '1', {'packages':['corechart', 'table', 'gauge']});
 	google.setOnLoadCallback(reporte); 
- </script>
-<div id="report" class="">
-    <div id="leadInfo">
-        <ul id="leadData">
+ </script> 
+    <dl class="customReport">
+		<dt class="tituloForm">Informacion del Estudiante</dt>
+        <dd>
+            <ul id="leadData">
+            </ul>
+        </dd>
+        <dt class="tituloForm">Auto Perfil Educativo del Estudiante</dt>
+        <dd>
+            <div id="vSiNo-1"></div>
+            <div id="vDT-DS1"></div>
+            <div id="vGVel-DS2"></div>
+            <div id="vDT-DS3"></div>
+            <div id="vDT-DS4"></div>
+        </dd>
+		<dt class="tituloForm">Intereses y Actividades Generales</dt>
+        <dd>
+            <div id="vSiNo-2"></div>
+            <div id="vSiNo-3"></div>
+            <div id="vSiNo-4"></div>
+        </dd>
         
-        </ul>
-    </div>
-    
-    <br>    <br>    <br>
-    
-    <div id="visualization"></div>
-    <div id="visualizationb"></div>
-    <div id="visualizationc"></div>
-    <div id="visualizationd"></div>
-</div>
+        <dt class="tituloForm">Intereses y Metas de Estudios Generales</dt>
+        <dd>
+            <div id="vDT-DS5"></div>
+            <div id="vDT-DS6"></div>
+            <div id="vPC-DS7"></div>
+        </dd>
+        
+    </dl>
+
 <?php get_footer(); ?>
