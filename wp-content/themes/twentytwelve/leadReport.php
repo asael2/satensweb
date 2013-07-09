b2021413dd3571079046cddc517a1b9333ed5119
asael2 <asael2@gmail.com>
2013-07-08 23:08:25 -0300

    styles everywhere ok

--- wp-content/themes/twentytwelve/leadReport.php
+++ wp-content/themes/twentytwelve/leadReport.php
@@ -8,7 +8,6 @@ get_header(); ?>
 <div id="content" role="main">
 <?php while ( have_posts() ) : the_post(); ?>
 <?php get_template_part( 'content', 'page' ); ?>
-<?php comments_template( '', true ); ?>
 <?php endwhile; // end of the loop. ?>
 
 <!--Load the AJAX API--> 
