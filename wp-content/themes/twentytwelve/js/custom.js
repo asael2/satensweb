685d3b753371b7944fd0ef90531ef40fb2ea73b6
asael2 <asael2@gmail.com>
2013-07-09 20:42:21 -0300

    Refactoreado

--- wp-content/themes/twentytwelve/js/custom.js
+++ wp-content/themes/twentytwelve/js/custom.js
@@ -1,4 +1,4 @@
-var reporte, respuestas, respondido, leadId, pageType, entryTitle, preguntaDom, editLink, sumOfFields;
+var reporte; //respuestas, this.respondido, leadId, pageType, entryTitle, preguntaDom, editLink, sumOfFields;
 
 $.urlParam = function (name) {
 	var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(window.location.href);
@@ -7,45 +7,6 @@ $.urlParam = function (name) {
 	}else{
 		return results[1] || 0;
 	}
-};
-
-preguntaDom = function (DOMnumber) {
-	var label = $(".entry-view-field-name").eq(DOMnumber).text();
-	//console.log(label);
-	return label;
-};
-
-respondido = function (fieldN) {
-	var fn, fvalue, fieldNumber, lares;
-	fieldNumber = fieldN;
-	$.each(respuestas, function(index){
-		fn = this["field_number"];
-		fvalue = this["value"];
-		if (fieldNumber == fn){ 
-           lares = fvalue;
-           return false
-		}
-	});		
-    return lares;
-};
-
-
-
-sumOfFields = function (fNumber, optsNumber) {
-	//Establecer un metodo para obtener el correspondiente 	
-	//optsNumber :  posibles respuestas
-	var elvalue, i;
-	var suma =0;
-	
-	for(i=1; i<=optsNumber; i++){
-		elvalue = parseInt(respondido(fNumber+"."+i));
-		if (!elvalue == 1){
-			elvalue = 0;
-		}else{
-			suma = elvalue + suma;
-		}
-	}
-	return suma;
 };
 
 //Classes////////////////////////////////////////////////////
@@ -127,47 +88,47 @@ reporte = {
 
 	startDraw : function () {
 		//Page Tittle
-		$("h1.entry-title").html( "Reportes de Estudiante "+ respondido(3) );
+		$("h1.entry-title").html( "Reportes de Estudiante "+ this.respondido(3) );
 
 		//SERAID, Field numbers 1
-		studentBasicData("SERA ID" , respondido(1));	
+		studentBasicData("SERA ID" , this.respondido(1));	
 		//Info del estudiante,Field numbers 3-6
-	    studentBasicData("Nombre " , respondido(3));
-	    studentBasicData("Segundo Nombre " , respondido(4));
-	    studentBasicData("Apellido " , respondido(5));
-	    studentBasicData("Segundo Apellido " , respondido(6));
-	    studentBasicData("Genero " , respondido(7));
-	    studentBasicData("Fecha de nacimiento " , respondido(8));
-	    studentBasicData("Grado ", respondido(9));
+	    studentBasicData("Nombre " , this.respondido(3));
+	    studentBasicData("Segundo Nombre " , this.respondido(4));
+	    studentBasicData("Apellido " , this.respondido(5));
+	    studentBasicData("Segundo Apellido " , this.respondido(6));
+	    studentBasicData("Genero " , this.respondido(7));
+	    studentBasicData("Fecha de nacimiento " , this.respondido(8));
+	    studentBasicData("Grado ", this.respondido(9));
 		studentBasicData("Editar ", this.editLink());
 	   
 	 	//Form Beak<<< Auto Perfil Educativo del Estudiante
 
 	 	//CHART
-		siNoColumnChart("Recibe servicios en la escuela por algunas dificultades académicas, problemas de salud o impedimento", respondido(22), 'vSiNo-1')
+		siNoColumnChart("Recibe servicios en la escuela por algunas dificultades académicas, problemas de salud o impedimento", this.respondido(22), 'vSiNo-1')
 
 		//DATATABLE      
 		var dataSet1 = [
 				//Columnas
 				['Servicios que recibe en la escuela', 'Recibe'],
 				//Filas
-				['Consejero Escolar', 		(respondido(23.1) == "true") ], 
-				['Sicólogo', 				(respondido(23.2) == "true") ], 
-				['Tutoría', 				(respondido(23.3) == "true") ],
-				['Terapia de Habla',  		(respondido(23.4) == "true") ],
-				['Terapia Ocupacional',  	(respondido(23.5) == "true") ],
-				['Asistencia Tecnológica',	(respondido(23.6) == "true") ],
-				['Acomodos', 				(respondido(23.7) == "true") ],
-				['Otros',  					(respondido(23.8) == "true") ]
+				['Consejero Escolar', 		(this.respondido(23.1) == "true") ], 
+				['Sicólogo', 				(this.respondido(23.2) == "true") ], 
+				['Tutoría', 				(this.respondido(23.3) == "true") ],
+				['Terapia de Habla',  		(this.respondido(23.4) == "true") ],
+				['Terapia Ocupacional',  	(this.respondido(23.5) == "true") ],
+				['Asistencia Tecnológica',	(this.respondido(23.6) == "true") ],
+				['Acomodos', 				(this.respondido(23.7) == "true") ],
+				['Otros',  					(this.respondido(23.8) == "true") ]
 		]; 
 		laDataTable(dataSet1, 'vDT-DS1');
 		
 		//VELOCIMETROS
 		var dataSet2 = [
 			['Destreza',	'Valor'],
-			['Lectura', 	parseInt(respondido(24))],
-			['Escritura',	parseInt(respondido(25))],
-			['Matemáticas',	parseInt(respondido(26))]
+			['Lectura', 	parseInt(this.respondido(24))],
+			['Escritura',	parseInt(this.respondido(25))],
+			['Matemáticas',	parseInt(this.respondido(26))]
 		]; 
 		velocimetros(dataSet2, 'vGVel-DS2');
 
@@ -177,10 +138,10 @@ reporte = {
 			['Autocalificación ', 'Lectura', 'Escritura', 'Matematicas'],
 			
 			//Filas
-			['Desconozco',				(respondido(24) == 0), (respondido(25) == 0), (respondido(26) == 0)],
-	      	['Por debajo de los demás', (respondido(24) == 1), (respondido(25) == 1), (respondido(26) == 1)],
-	      	['Igual que los demás',  	(respondido(24) == 2), (respondido(25) == 2), (respondido(26) == 2)],
-		  	['Por encima de los demás', (respondido(24) == 3), (respondido(25) == 3), (respondido(26) == 3)]
+			['Desconozco',				(this.respondido(24) == 0), (this.respondido(25) == 0), (this.respondido(26) == 0)],
+	      	['Por debajo de los demás', (this.respondido(24) == 1), (this.respondido(25) == 1), (this.respondido(26) == 1)],
+	      	['Igual que los demás',  	(this.respondido(24) == 2), (this.respondido(25) == 2), (this.respondido(26) == 2)],
+		  	['Por encima de los demás', (this.respondido(24) == 3), (this.respondido(25) == 3), (this.respondido(26) == 3)]
 		]; 
 		laDataTable(dataSet3, 'vDT-DS3');
 		
@@ -189,22 +150,22 @@ reporte = {
 			//Columnas
 			['Manera de confrontar necesidades académicas', 'Seleccion'],
 			//Filas
-			['Pienso en varias soluciones', 										(respondido(27.1) == "true") ],
-			['Busco consejo de otros', 												(respondido(27.2) == "true") ],
-			['Comienzo a resolver el probelma sin tener un plan', 					(respondido(27.3) == "true") ],
-			['Considero las cosas positivas y negativas del problema', 				(respondido(27.4) == "true") ],
-			['Considero otra solución cuando la primera idea no parece funcionar',	(respondido(27.5) == "true") ]
+			['Pienso en varias soluciones', 										(this.respondido(27.1) == "true") ],
+			['Busco consejo de otros', 												(this.respondido(27.2) == "true") ],
+			['Comienzo a resolver el probelma sin tener un plan', 					(this.respondido(27.3) == "true") ],
+			['Considero las cosas positivas y negativas del problema', 				(this.respondido(27.4) == "true") ],
+			['Considero otra solución cuando la primera idea no parece funcionar',	(this.respondido(27.5) == "true") ]
 		]; 
 		laDataTable(dataSet4, 'vDT-DS4');
 
 		//Form Break<<<Intereses y Actividades Generales
 
 		//CHART 
-		siNoColumnChart("Diversión en Tiempo Libre", respondido(29), 'vSiNo-2');
+		siNoColumnChart("Diversión en Tiempo Libre", this.respondido(29), 'vSiNo-2');
 		//CHART 
-		siNoColumnChart("Tareas y Responsabilidades en el Hogar", respondido(30), 'vSiNo-3');
+		siNoColumnChart("Tareas y Responsabilidades en el Hogar", this.respondido(30), 'vSiNo-3');
 		//CHART
-		siNoColumnChart("Aspiración a una Carrera en el Futuro", respondido(31), 'vSiNo-4');
+		siNoColumnChart("Aspiración a una Carrera en el Futuro", this.respondido(31), 'vSiNo-4');
 
 		//Form Break<<<Intereses y Metas de Estudios Generales
 
@@ -213,12 +174,12 @@ reporte = {
 			//Columnas
 			['Estudios que toma al presente o interesa tomar en la escuela superior', 'Seleccion'],
 			//Filas
-			['No Aplica', 								(respondido(41) == 0)],
-			['Programa de Preparación Universitaria', 	(respondido(41) == 1)],
-			['Programa General', 						(respondido(41) == 2)],
-			['Certificado Técnico / Vocaional', 		(respondido(41) == 3)],
-			['Programa de Estudio y Trabajo', 			(respondido(41) == 4)],
-			['Otro', 									(respondido(41) == 5)]
+			['No Aplica', 								(this.respondido(41) == 0)],
+			['Programa de Preparación Universitaria', 	(this.respondido(41) == 1)],
+			['Programa General', 						(this.respondido(41) == 2)],
+			['Certificado Técnico / Vocaional', 		(this.respondido(41) == 3)],
+			['Programa de Estudio y Trabajo', 			(this.respondido(41) == 4)],
+			['Otro', 									(this.respondido(41) == 5)]
 		]; 
 		laDataTable(dataSet5, 'vDT-DS5');
 		
@@ -227,12 +188,12 @@ reporte = {
 			//Columnas
 			['Estudios que aspira seguir cuando termine la escuela superior', 'Seleccion'],
 			//Filas
-			['No Aplica',						(respondido(42) == 0 )],
-			['Instituto Técnico Vocacional',	(respondido(42) == 1 )],
-			['Grado Asociado', 					(respondido(42) == 2 )],
-			['Bachillerato de Universidad',		(respondido(42) == 3 )],
-			['Programa de Estudios Cortos',		(respondido(42) == 4 )],
-			['Ninguna de las Anteriores',		(respondido(42) == 5 )]
+			['No Aplica',						(this.respondido(42) == 0 )],
+			['Instituto Técnico Vocacional',	(this.respondido(42) == 1 )],
+			['Grado Asociado', 					(this.respondido(42) == 2 )],
+			['Bachillerato de Universidad',		(this.respondido(42) == 3 )],
+			['Programa de Estudios Cortos',		(this.respondido(42) == 4 )],
+			['Ninguna de las Anteriores',		(this.respondido(42) == 5 )]
 		]; 
 		laDataTable(dataSet6, 'vDT-DS6');
 
@@ -250,11 +211,49 @@ reporte = {
 	}
 };//end Reporte
 
+
 reporte.editLink = function () {
 
 	return  $(".useredit").html()
 };
 
+reporte.sumOfFields = function (fNumber, optsNumber) {
+	//Establecer un metodo para obtener el correspondiente 	
+	//optsNumber :  posibles respuestas
+	var elvalue, i;
+	var suma =0;
+	
+	for(i=1; i<=optsNumber; i++){
+		elvalue = parseInt(this.respondido(fNumber+"."+i));
+		if (!elvalue == 1){
+			elvalue = 0;
+		}else{
+			suma = elvalue + suma;
+		}
+	}
+	return suma;
+};
+
+reporte.respondido = function (fieldN) {
+	var fn, fvalue, fieldNumber, lares;
+	fieldNumber = fieldN;
+	$.each(respuestas, function(index){
+		fn = this["field_number"];
+		fvalue = this["value"];
+		if (fieldNumber == fn){ 
+           lares = fvalue;
+           return false
+		}
+	});		
+    return lares;
+};
+/*
+reporte.preguntaDom = function (DOMnumber) {
+	var label = $(".entry-view-field-name").eq(DOMnumber).text();
+	//console.log(label);
+	return label;
+};
+*/
 //////onReady
 $(function () {
 	//SERAiD
