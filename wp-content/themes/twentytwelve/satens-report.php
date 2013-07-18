b42197231056a672bb668ebc44280741f896b882
asael2 <asael2@gmail.com>
2013-07-18 00:07:57 -0300

    Base 1

--- wp-content/themes/twentytwelve/satens-report.php
+++ wp-content/themes/twentytwelve/satens-report.php
@@ -16,59 +16,49 @@
             <li><a href="#tabs-8">Reporte 8 </a></li>
             <li><a href="#tabs-9">Reporte 9 </a></li>
             <li><a href="#tabs-10">Reporte 10</a></li>
+            <li><a href="#tabs-11">Reporte 11</a></li>
         </ul>
-        <div id="tabs-1">
-        Formulario No. 1
+        <div id="tabs-1"> Formulario No. 1
             <ul class="form1">
             </ul>
         </div>
-        <div id="tabs-2">
-        Formulario No. 2
+        <div id="tabs-2"> Formulario No. 2
             <ul class="form2">
             </ul>
         </div>
-        <div id="tabs-3">
-        Formulario No. 3
+        <div id="tabs-3"> Formulario No. 3
             <ul class="form3">
             </ul>
         </div>
-        <div id="tabs-4">
-        Formulario No. 4
+        <div id="tabs-4"> Formulario No. 4
             <ul class="form4">
             </ul>
         </div>
-        <div id="tabs-5">
-        Formulario No. 5
+        <div id="tabs-5"> Formulario No. 5
             <ul class="form5">
             </ul>
         </div>
-        <div id="tabs-6">
-        Formulario No. 6
+        <div id="tabs-6"> Formulario No. 6
             <ul class="form6">
             </ul>
         </div>
-        <div id="tabs-7">
-        Formulario No. 7
+        <div id="tabs-7"> Formulario No. 7
             <ul class="form7">
             </ul>
         </div>
-        <div id="tabs-8">
-        Formulario No. 8
+        <div id="tabs-8"> Formulario No. 8
             <ul class="form8">
             </ul>
         </div>
-        <div id="tabs-9">
-        Formulario No. 9
+        <div id="tabs-9"> Formulario No. 9
             <ul class="form9">
             </ul>
         </div>
-        <div id="tabs-10">
-        Formulario No. 10
+        <div id="tabs-10"> Formulario No. 10
             <ul class="form10">
             </ul>
         </div>
-        <div id="tabs-11">
-        Formulario No. 11
+        <div id="tabs-11"> Formulario No. 11
             <ul class="form11">
             </ul>
         </div>
