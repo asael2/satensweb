a30745061f2187f1de52f390d252587d43fe5a5d
asael2 <asael2@gmail.com>
2013-07-18 20:28:24 -0300

    Titulo tab formularios

--- wp-content/themes/twentytwelve/satens-report.php
+++ wp-content/themes/twentytwelve/satens-report.php
@@ -18,47 +18,47 @@
             <li><a href="#tabs-10">Reporte 10</a></li>
             <li><a href="#tabs-11">Reporte 11</a></li>
         </ul>
-        <div id="tabs-1"> Formulario No. 1
+        <div id="tabs-1"> Informe 1: Auto-Perfil Educativo del Estudiante, Actividades Generales e Intereses
             <ul class="form1">
             </ul>
         </div>
-        <div id="tabs-2"> Formulario No. 2
+        <div id="tabs-2"> Informe 2: Cuestionario al Padre o Encargado sobre los Planes de Estudio y Trabajo del Estudiante
             <ul class="form2">
             </ul>
         </div>
-        <div id="tabs-3"> Formulario No. 3
+        <div id="tabs-3"> Informe 3: Auto-Funcionamiento que el Estudiante Identifica Tiene en la Escuela al Presente y Planes Futuros
             <ul class="form3">
             </ul>
         </div>
-        <div id="tabs-4"> Formulario No. 4
+        <div id="tabs-4"> Informe 4: Perspectiva y Visión del Padre sobre el Diario Vivir y Actividades Comunitarias del Estudiante
             <ul class="form4">
             </ul>
         </div>
-        <div id="tabs-5"> Formulario No. 5
+        <div id="tabs-5"> Informe 5: Estudiante | Auto Conocimiento de Información sobre su Necesidad Especial/Discapacidad
             <ul class="form5">
             </ul>
         </div>
-        <div id="tabs-6"> Formulario No. 6
+        <div id="tabs-6"> Informe 6: Avalúo de Destrezas Básicas Ocupacionales (Oferta de Empleos, Principios de Manejo de Cuenta de Cheques y Dinero, Gráficas, Medidas y Estimado de Salario por Trabajo)
             <ul class="form6">
             </ul>
         </div>
-        <div id="tabs-7"> Formulario No. 7
+        <div id="tabs-7"> Informe 7: Áreas de Destrezas Funcionales Identificadas por el Estudiante y su Padre*, Madre* o Encargado* para Atenderse Durante Año Escolar 
             <ul class="form7">
             </ul>
         </div>
-        <div id="tabs-8"> Formulario No. 8
+        <div id="tabs-8"> Informe 8: Evaluación* de Destrezas del Diario Vivir, Cocina y Preparación de Alimentos, Lavado de Ropa, Convivencia Familiar ,  Limpieza en el Hogar, Limpieza del Patio, Asuntos Personales, Salud, Comunitarias, Tiempo Libre, Planes ocupacionales futuros y vivienda. 
             <ul class="form8">
             </ul>
         </div>
-        <div id="tabs-9"> Formulario No. 9
+        <div id="tabs-9"> Informe 9: Estudiante-Padre Apreciaciación de la Ayuda para las Clases, Tomar Exámenes, Problemas de Lectura, Escritura  y Matemáticas
             <ul class="form9">
             </ul>
         </div>
-        <div id="tabs-10"> Formulario No. 10
+        <div id="tabs-10"> Informe 10: Estudiante - Intereses y Preferencias 
             <ul class="form10">
             </ul>
         </div>
-        <div id="tabs-11"> Formulario No. 11
+        <div id="tabs-11"> Informe 11: Estudiante - Auto-avaluó de Destrezas y Capacidades Generales
             <ul class="form11">
             </ul>
         </div>
