cf99dba1091d807ee93ce52995cb30e09011ddd0
asael2 <asael2@gmail.com>
2013-07-25 23:44:07 -0300

    Scroll implemented

--- wp-content/themes/twentytwelve/js/custom.js
+++ wp-content/themes/twentytwelve/js/custom.js
@@ -301,4 +301,23 @@ $(function () {
 		})
 			
 	}//Editar eof
+
+
+});
+
+ $(document).ready(function(){ 
+ 
+        $(window).scroll(function(){
+            if ($(this).scrollTop() > 100) {
+                $('.scrollup').fadeIn();
+            } else {
+                $('.scrollup').fadeOut();
+            }
+        }); 
+ 
+        $('.scrollup').click(function(){
+            $("html, body").animate({ scrollTop: 0 }, 600);
+            return false;
+        });
+
 });
