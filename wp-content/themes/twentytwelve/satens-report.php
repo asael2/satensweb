fb238f0c2f6c161b9265b0bcfb96086a88e7201a
asael2 <asael2@gmail.com>
2013-07-28 05:23:29 -0300

    Fixes fixes

--- wp-content/themes/twentytwelve/satens-report.php
+++ wp-content/themes/twentytwelve/satens-report.php
@@ -1,14 +1,20 @@
 <div id="satensReport">
+
     <div class="wideSec">
-        <ul id="leadData">
-        </ul>
+        
+        <ul id="leadData"></ul>
+        
         <div id="leadPic"></div>
+        
         <ul id="leadTools">
             <li><a id="editLead" href="#">Editar</a></li>
             <li><a id="deleteLead" href="#">Eliminar</a></li>
 		</ul>
+        
     </div>
+
     <div id="tabsInforme">
+
         <ul id="tabsMenu">
             <li><a href="#tabs-1">Reporte 1 </a></li>
             <li><a href="#tabs-2">Reporte 2 </a></li>
@@ -22,60 +28,63 @@
             <li><a href="#tabs-10">Reporte 10</a></li>
             <li><a href="#tabs-11">Reporte 11</a></li>
         </ul>
+
         <div id="tabs-1">
             <h3>Reporte 1: Auto-Perfil Educativo del Estudiante, Actividades Generales e Intereses</h3>
-            <ul class="form1">
-            </ul>
+            <ul class="form1"></ul>
         </div>
+
         <div id="tabs-2">
             <h3>Reporte 2: Cuestionario al Padre o Encargado sobre los Planes de Estudio y Trabajo del Estudiante</h3>
-            <ul class="form2">
-            </ul>
+            <ul class="form2"></ul>
         </div>
+
         <div id="tabs-3">
             <h3>Reporte 3: Auto-Funcionamiento que el Estudiante Identifica Tiene en la Escuela al Presente y Planes Futuros</h3>
             <ul class="form3">
             </ul>
         </div>
+
         <div id="tabs-4">
             <h3>Reporte 4: Perspectiva y Visión del Padre sobre el Diario Vivir y Actividades Comunitarias del Estudiante </h3>
-            <ul class="form4">
-            </ul>
+            <ul class="form4"></ul>
         </div>
+
         <div id="tabs-5">
             <h3>Reporte 5: Estudiante | Auto Conocimiento de Información sobre su Necesidad Especial/Discapacidad</h3>
-            <ul class="form5">
-            </ul>
+            <ul class="form5"> </ul>
         </div>
+
         <div id="tabs-6">
             <h3>Reporte 6: Avalúo de Destrezas Básicas Ocupacionales (Oferta de Empleos, Principios de Manejo de Cuenta de Cheques y Dinero, Gráficas, Medidas y Estimado de Salario por Trabajo)</h3>
-            <ul class="form6">
-            </ul>
+            <ul class="form6"></ul>
         </div>
+
         <div id="tabs-7">
             <h3>Reporte 7: Áreas de Destrezas Funcionales Identificadas por el Estudiante y su Padre*, Madre* o Encargado* para Atenderse Durante Año Escolar </h3>
-            <ul class="form7">
-            </ul>
+            <ul class="form7"></ul>
         </div>
+
         <div id="tabs-8">
             <h3>Reporte 8: Evaluación* de Destrezas del Diario Vivir, Cocina y Preparación de Alimentos, Lavado de Ropa, Convivencia Familiar ,  Limpieza en el Hogar, Limpieza del Patio, Asuntos Personales, Salud, Comunitarias, Tiempo Libre, Planes ocupacionales futuros y vivienda. </h3>
-            <ul class="form8">
-            </ul>
+            <ul class="form8"></ul>
         </div>
+
         <div id="tabs-9">
             <h3>Reporte 9: Estudiante-Padre Apreciaciación de la Ayuda para las Clases, Tomar Exámenes, Problemas de Lectura, Escritura  y Matemáticas </h3>
-            <ul class="form9">
-            </ul>
+            <ul class="form9"></ul>
         </div>
+
         <div id="tabs-10">
             <h3> Reporte 10: Estudiante - Intereses y Preferencias </h3>
-            <ul class="form10">
-            </ul>
+            <ul class="form10"></ul>
         </div>
+ 
         <div id="tabs-11">
             <h3>Reporte 11: Estudiante - Auto-avaluó de Destrezas y Capacidades Generales </h3>
-            <ul class="form11">
-            </ul>
+            <ul class="form11"></ul>
         </div>
+        
     </div>
+    
 </div>
